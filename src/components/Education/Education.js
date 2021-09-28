e9b6feee5048cd7b0d7f7d36f746b06a2d05ef90
AnujaOswal <Oswalanujak@gmail.com>
2021-09-28 22:45:02 +0530

    inital commit

---
+++ src/components/Education/Education.js
@@ -0,0 +1,22 @@
+import { education } from '../../portfolio'
+import './Education.css'
+
+const Education = () => {
+  const { school, studyspecialization, yearduration, score } = education
+  return (
+    <div>
+      <h2 className='section__title' style={{ paddingTop: '0.8em' }}>
+        Education
+      </h2>
+      <p style={{ padding: '0.2em' }}>
+        My name is Anuja oswal. I’m a graduate of {yearduration} from {school}
+        at Baramati with a degree in {studyspecialization} and scored {score}
+        CGPA. I m most passionate about giving back to the community, and my
+        goal is to pursue this passion within the field of software engineering.
+        In my free time I like working on open source projects.
+      </p>
+    </div>
+  )
+}
+
+export default Education
---
+++ src/portfolio.js
@@ -0,0 +1,104 @@
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  homepage: 'https://AnujaOswal.github.io/cleanfolio',
+  title: 'AO.',
+}
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'Anuja Oswal',
+  role: 'Aspring Developer ',
+  description: 'curious learner',
+  resume: 'https://example.com',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/anuja-oswal-783476128',
+    github: 'https://github.com/AnujaOswal',
+  },
+}
+
+const education = {
+  school:
+    'Vidya Pratishthan Kamalnayan Bajaj Institute of Engineering & Technology',
+  studyspecialization: 'Bachelor Of Enginnering in Computer',
+  yearduration: '2020',
+  score: '7.01',
+}
+
+const projects = [
+  // projects can be added an removed
+  // if there are no projects, Projects section won't show up
+  {
+    name: 'Whatsapp Chating Clone',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['ReactJs', 'CSS', 'firebase'],
+    sourceCode: 'https://github.com/AnujaOswal/WhatsappReactClone',
+    livePreview: 'https://whatsappweb-ao.netlify.app/',
+  },
+  {
+    name: 'Netflix Video Streaming Clone',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['ReactJs', 'CSS', 'Firebase'],
+    sourceCode: 'https://github.com/AnujaOswal/NetFlix-CLONE',
+    livePreview: 'https://netflix-video-streaming-clone.netlify.app/',
+  },
+  {
+    name: 'Cryptocurrency Finance Price Tracker',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['CSS', 'Bootstrap', 'ReactJs'],
+    sourceCode:
+      'https://github.com/AnujaOswal/Cryptocurrency-Finance-Price-Tracker-React-App-',
+    livePreview: 'https://cryptocurrency-pricetracker-app.netlify.app',
+  },
+  // {
+  //   name: 'Project 4',
+  //   description:
+  //     'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+  //   stack: ['SASS', 'TypeScript', 'React'],
+  //   sourceCode: 'https://github.com',
+  //   livePreview: 'https://github.com',
+  // },
+  // {
+  //   name: 'Project 5',
+  //   description:
+  //     'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+  //   stack: ['SASS', 'TypeScript', 'React'],
+  //   sourceCode: 'https://github.com',
+  //   livePreview: 'https://github.com',
+  // },
+  // {
+  //   name: 'Project 6',
+  //   description:
+  //     'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+  //   stack: ['SASS', 'TypeScript', 'React'],
+  //   sourceCode: 'https://github.com',
+  //   livePreview: 'https://github.com',
+  // },
+]
+
+const skills = [
+  // skills can be added or removed
+  // if there are no skills, Skills section won't show up
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'Bootstrap',
+  'React',
+  'Redux',
+  'Material UI',
+  'MongoDB',
+  'MySql',
+  'NodeJs',
+  'Git',
+  'Java',
+]
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: 'anujaoswal7@gmail.com',
+  desc: 'My Inbox is always open. Whether you have a question or just want to say hello. I will try my best to get back to you! Feel free to mail me about any relevant job updates.',
+}
+
+export { header, about, projects, skills, contact, education }
